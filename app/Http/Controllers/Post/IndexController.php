d1743293966e3048a1d5f2aef60585fc53bdba4c
Tim2788 <Sologov88@mail.ru>
2022-04-22 23:11:29 +0300

    23.11

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Post;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -11,10 +12,19 @@ class IndexController extends Controller
 {
    public function __invoke(){
 
+      // $users = User::all();      //подсчет количества доменов электронной почты
+      // foreach ($users as $user){
+      //   $coll[] = $user->email;
+      // }
+      // $collection = collect($coll);
+      // $counted = $collection->countBy(function ($email) {
+      //    return substr(strrchr($email, "@"), 1);
+      //    });
+      //     dd( $counted->all());
+      
 
 
-
-       $posts = Post::paginate(1);
+       $posts = Post::paginate(3);
        $postsRandom = Post::get()->random(2);
       $postsPopular = Post::withCount('postPopular')->orderBy('post_popular_count', 'DESC')->get()->take(4);
 
--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 
 class ShowController extends Controller
 {
@@ -14,18 +15,24 @@ class ShowController extends Controller
 
        $date = Carbon::parse($post->created_at);
 
+      //  $relatedPosts = Post::where('category_id', $post->category_id)
+      //  ->where('id', '!=', $post->id)
+      //  ->get()
+      //  ->take(3);
 
-       $relatedPosts = Post::where('category_id', $post->category_id)
-       ->where('id', '!=', $post->id)
-       ->get()
-       ->take(3);
+      // dd( $post->tags->modelKeys('tag_id'));
 
-// //Test
-//        $relatedPosts = Post::where('category_id', $post->category_id)
-//        ->where('id', '!=', $post->id)
-//        ->get()
-//        ->take(3);
+      // dd(Post::whereIn('category_id', [1] )->get());
 
+      $relatedPosts = Post::whereHas('tags', fn (Builder $query) => $query
+      ->whereIn('tag_id', $post->tags->modelKeys('tag_id'))
+      ->where('post_id', '!=', $post->tags->modelKeys('tag_id')))
+      ->get()->take(3);
+      
+
+
+
+     
 
 return view('post.show', compact('post', 'date', 'relatedPosts'));
 
